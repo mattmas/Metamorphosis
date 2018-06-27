539321963eda09cb990b56ede4925749bbe65c1e
MMason <mmason@rand.com>
2018-06-27 16:25:54 -0400

    Fix issues with batch processing, both exception handling and category issues.

--- src/RevitFingerPrint/Commands/Compare.cs
+++ src/RevitFingerPrint/Commands/Compare.cs
@@ -154,89 +154,108 @@ namespace Metamorphosis
         /// <returns></returns>
         public static string BatchCompare(Document doc, string previousFile, string targetFolder, bool dateStampResults, string categoryConfig, out int numChanges)
         {
-            doc.Application.WriteJournalComment("Launching Batch Metamorphosis Compare:", false);
-            doc.Application.WriteJournalComment("  Previous File: " + previousFile, false);
-            doc.Application.WriteJournalComment("  TargetFolder:  " + targetFolder, false);
-            doc.Application.WriteJournalComment("  Date Stamp Results: " + dateStampResults, false);
-            doc.Application.WriteJournalComment("  Category Config: " + categoryConfig, false);
-            ExternalApp.FirstTimeRun(); // analytics
-
-            numChanges = -1;
-            if (previousFile == null)
+            try
             {
-                Compare c = new Compare();
-                previousFile = c.GetFilenameHint(doc);
-            }
+                doc.Application.WriteJournalComment("Launching Batch Metamorphosis Compare:", false);
+                doc.Application.WriteJournalComment("  Previous File: " + previousFile, false);
+                doc.Application.WriteJournalComment("  TargetFolder:  " + targetFolder, false);
+                doc.Application.WriteJournalComment("  Date Stamp Results: " + dateStampResults, false);
+                doc.Application.WriteJournalComment("  Category Config: " + categoryConfig, false);
+                ExternalApp.FirstTimeRun(); // analytics
 
-            ComparisonMaker comparison = new ComparisonMaker(doc, previousFile);
-            comparison.AllCategories = (categoryConfig == null || categoryConfig.ToUpper() == "[ALL]" || categoryConfig.ToUpper() == "ALL");
-            if (!comparison.AllCategories)
-            {
-                if (categoryConfig.Contains("["))
+                numChanges = -1;
+                if (previousFile == null)
                 {
-                    // figure that it's a special configuration.
-                    string typeName = categoryConfig.Trim('[', ']', ' ');
-                    CategoryType targetType = CategoryType.Model;
+                    Compare c = new Compare();
+                    previousFile = c.GetFilenameHint(doc);
+                }
 
-                    if (Enum.TryParse<CategoryType>(typeName, out targetType))
+                ComparisonMaker comparison = new ComparisonMaker(doc, previousFile);
+                comparison.AllCategories = (categoryConfig == null || categoryConfig.ToUpper() == "[ALL]" || categoryConfig.ToUpper() == "ALL");
+                if (!comparison.AllCategories)
+                {
+                    if (categoryConfig.Contains("["))
                     {
-                        foreach (Category cat in doc.Settings.Categories)
+                        // figure that it's a special configuration.
+                        string typeName = categoryConfig.Trim('[', ']', ' ');
+                        CategoryType targetType = CategoryType.Model;
+
+                        if (Enum.TryParse<CategoryType>(typeName, out targetType))
                         {
-                            if (cat.CategoryType == targetType) comparison.RequestedCategories.Add(cat);
+                            foreach (Category cat in doc.Settings.Categories)
+                            {
+                                if (cat.CategoryType == CategoryType.Internal) continue;
+                                if (cat.CategoryType == CategoryType.Invalid) continue;
+
+                                if (cat.CategoryType == targetType) comparison.RequestedCategories.Add(cat);
+                            }
+                        }
+                        else
+                        {
+                            throw new ApplicationException("Did not find dynamic Category Filter: " + typeName);
+
                         }
                     }
-                    else
+                    else  // look for a file-based thing.
                     {
-                        throw new ApplicationException("Did not find dynamic Category Filter: " + typeName);
+                        // we need to go look for the stored settings with this name...
+                        var file = CategorySettingsFile.GetFileByName(categoryConfig);
 
-                    }
-                }
-                else  // look for a file-based thing.
-                {
-                    // we need to go look for the stored settings with this name...
-                    var file = CategorySettingsFile.GetFileByName(categoryConfig);
-
-                    doc.Application.WriteJournalComment("Found CategorySettingsFile: " + file.Filename, false);
-                    comparison.RequestedCategories = new List<Category>();
-                    foreach (var setting in file.Settings)
-                    {
-                        if (setting.Enabled)
+                        doc.Application.WriteJournalComment("Found CategorySettingsFile: " + file.Filename, false);
+                        comparison.RequestedCategories = new List<Category>();
+                        foreach (var setting in file.Settings)
                         {
-                            Category c = doc.Settings.Categories.get_Item((BuiltInCategory)setting.CategoryId);
-                            if (c != null) comparison.RequestedCategories.Add(c);
+                            if (setting.Enabled)
+                            {
+                                Category c = doc.Settings.Categories.get_Item((BuiltInCategory)setting.CategoryId);
+                                if (c != null)
+                                {
+                                    if (c.CategoryType == CategoryType.Internal) continue;
+                                    if (c.CategoryType == CategoryType.Invalid) continue;
+
+                                    comparison.RequestedCategories.Add(c);
+                                }
+                            }
                         }
                     }
+                    doc.Application.WriteJournalComment("Have " + comparison.RequestedCategories.Count + " categories requested for comparison.", false);
+
                 }
-                doc.Application.WriteJournalComment("Have " + comparison.RequestedCategories.Count + " categories requested for comparison.", false);
-                
-            }
 
-            string tmpFile = doc.Title;
-            if (tmpFile.ToUpper().Contains("_DETACHED")) tmpFile = tmpFile.ToLower().Replace("_detached", "");
+                string tmpFile = doc.Title;
+                if (tmpFile.ToUpper().Contains("_DETACHED")) tmpFile = tmpFile.ToLower().Replace("_detached", "");
 
-            doc.Application.WriteJournalComment("Starting comparison...", false);
+                doc.Application.WriteJournalComment("Starting comparison...", false);
 
-            IList<Objects.Change> changes = comparison.Compare();
+                IList<Objects.Change> changes = comparison.Compare();
 
-            doc.Application.WriteJournalComment("Found " + changes.Count + " change(s).", false);
-            numChanges = changes.Count;
+                doc.Application.WriteJournalComment("Found " + changes.Count + " change(s).", false);
+                numChanges = changes.Count;
 
-            if (changes.Count > 0)
-            {
-                string jsonFile = Path.Combine(targetFolder, Path.GetFileNameWithoutExtension(tmpFile) + "-Changes-Latest.json");
-                comparison.Serialize(jsonFile, changes);
-                if (dateStampResults)
+                if (changes.Count > 0)
                 {
-                    jsonFile = Path.Combine(targetFolder, Path.GetFileNameWithoutExtension(tmpFile) + "-Changes-" + DateTime.Now.ToString("yyyyMMdd_hhmm") + ".json");
+                    string jsonFile = Path.Combine(targetFolder, Path.GetFileNameWithoutExtension(tmpFile) + "-Changes-Latest.json");
                     comparison.Serialize(jsonFile, changes);
+                    if (dateStampResults)
+                    {
+                        jsonFile = Path.Combine(targetFolder, Path.GetFileNameWithoutExtension(tmpFile) + "-Changes-" + DateTime.Now.ToString("yyyyMMdd_hhmm") + ".json");
+                        comparison.Serialize(jsonFile, changes);
+                    }
+
+
+                    return jsonFile;
                 }
-
-
-                return jsonFile;
+                else
+                {
+                    return null;
+                }
             }
-            else
+            catch (Exception ex)
             {
-                return null;
+                doc.Application.WriteJournalComment("Unexpected exception in Metamorphosis ModelCompare. " + ex.GetType().Name + ": " + ex.Message, false);
+                doc.Application.WriteJournalComment(ex.StackTrace, false);
+
+                throw;
             }
         }
 
